b2437757288c43bc4090db831e418b38bee8fb00
Martin Eduardo Miranda Mejias <maxgx@localhost.localdomain>
2021-07-23 23:19:10 -0400

    Esquema Basico de BackEnd

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CiudadModule } from './ciudad/ciudad.module';
+import { OrganizacionModule } from './organizacion/organizacion.module';
+import { InstitucionModule } from './institucion/institucion.module';
+import { CategoriaModule } from './categoria/categoria.module';
+import { SubcategoriaModule } from './subcategoria/subcategoria.module';
 
 @Module({
-  imports: [CiudadModule],
+  imports: [CiudadModule, OrganizacionModule, InstitucionModule, CategoriaModule, SubcategoriaModule],
   controllers: [AppController],
   providers: [AppService],
 })
