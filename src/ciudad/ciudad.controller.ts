033ec38c1430b37d487c3e4e4988b8996483b202
Martin Eduardo Miranda Mejias <maxgx@localhost.localdomain>
2021-07-23 21:12:12 -0400

    Avance CRUD ciudad

--- src/ciudad/ciudad.controller.ts
+++ src/ciudad/ciudad.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
 
 @Controller('ciudad')
-export class CiudadController {}
+export class CiudadController {
+    
+    @Get()
+    async getMany(){
+        return "No Habilitado"
+    }
+
+    @Get(':id')
+    async getCity(@Param('id',ParseIntPipe) idCity){
+        return "No Habilitado"
+    }
+
+    @Get('/region/:id')
+    async getByRegion(@Param('id') idRegion){
+        return "No Habilitado"
+    }
+
+    @Post()
+    async addCity(){
+        return "No Habilitado"
+    }
+
+    @Put()
+    async editCity(){
+        return "No Habilitado"
+    }
+
+    @Delete()
+    async deleteCity(){
+        return "No Habilitado"
+    }
+
+
+
+}
